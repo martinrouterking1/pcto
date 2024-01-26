e18307bbabc88780ed70d3723dc09313322b8ca4
martinrouterking1 <157354118+martinrouterking1@users.noreply.github.com>
2024-01-26 08:24:06 +0000

    a

--- startup/startup.c
+++ startup/startup.c
@@ -12,7 +12,8 @@
   4. Implementare il bilancio legato all'utente che riprendendo dalla funzione che genera profitto aggiunge soldi al bilancio dell'utente
   5. Ricontrollo generale del corretto funzionamento del codice
   6. Aggiungere tutti gli sleep per rendere più bello il programma 
-  7. Risolvere il problema alla riga 44*/
+  7. Risolvere il problema alla riga 44
+  8. Ritorno alla pagina di pro*/
 
 int len; 
 char car[] = "0123456789abcdefghijklmnoqprstuvwyzxABCDEFGHIJKLMNOQPRSTUYWVZX!#$&?";
@@ -69,8 +70,10 @@ void    registrazione(t_user *user, int *accreg)
     printf("\nUsername: ");
     scanf(" %s", &username);
     user->username = username;
+    printf("%s\n", username);
     printf("Password: ");
     scanf(" %s", &password);
+    printf("%s\n", password);
     user->password = password;
     printf("\nStiamo generando il tuo codice personale che dovrai utilizzare per accedere al tuo bilancio.\nQuesto codice è unico e non va condiviso per garantire la massima sicurezza del servizio.");
     sleep (1);
@@ -98,13 +101,18 @@ scanf("%s", bilancio);*/
 
 if (scelta == 1){
     possibilinvestimenti:
+    sleep(1);
     printf("\nEcco le possibilità di investimento create su misura per te.\nPremi il numero corrispondente all'azienda per ottenere più informazioni\n");
     printf("1. Salute al passo\n2. EcoMobility\n3. GreenGrown\n4. ReMind\n\n");
     int sceltaimpresa;
     scanf("%d", &sceltaimpresa);
         if (sceltaimpresa == 1){
             printf("\n\nSalute al passo\nSettore: Fitness e Benessere\nPiattaforma di fitness online che offre programmi personalizzati e monitoraggio della salute per aiutare le persone a raggiungere i loro obiettivi di fitness. \n");
-            printf("Sei sicuro di voler investire in questa azienda?\n1. Confermo\n2. Non confermo");
+            printf("\nPer continuare con l'operazione premere: 1\nPer visualizzare le altre proposte di investimento premere: 2\n\n");
+            int confermacontinuare;
+            scanf("%d", confermacontinuare);
+            if (confermacontinuare == 1){
+            printf("\nSei sicuro di voler investire in questa azienda?\n1. Confermo\n2. Non confermo");            
             int conferma; 
             scanf("%d", &conferma);
                     if (conferma == 1){
@@ -129,9 +137,17 @@ if (scelta == 1){
                     else if (conferma == 2){
                         goto possibilinvestimenti; 
                     }
+            }
+            else if (confermacontinuare == 1){
+                goto possibilinvestimenti;
+            }
         }
         if (sceltaimpresa == 2){
             printf("\n\nEcoMobility\nSettore: Trasporti e Mobilità\nServizio di noleggio di veicoli elettrici a lungo termine, promuovendo la mobilità sostenibile nelle città.\n ");
+            printf("\nPer continuare con l'operazione premere: 1\nPer visualizzare le altre proposte di investimento premere: 2\n\n");
+            int confermacontinuare;
+            scanf("%d", confermacontinuare);
+            if (confermacontinuare == 1){
             printf("Sei sicuro di voler investire in questa azienda?\n1. Confermo\n2. Non confermo");
             int conferma; 
             scanf("%d", &conferma);
@@ -158,10 +174,16 @@ if (scelta == 1){
                         goto possibilinvestimenti; 
                     }
                 }
-
+                else if (confermacontinuare == 1){
+                goto possibilinvestimenti;
+            }
+        }
         
         if (sceltaimpresa == 3){
             printf("\n\nGreenGrow\nSettore: Agricoltura e sostenibilità alimentare\nSistema automatizzato di coltivazione indoor per produrre ortaggi freschi e sostenibili direttamente in casa.\n ");
+            int confermacontinuare;
+            scanf("%d", confermacontinuare);
+            if (confermacontinuare == 1){
             printf("Sei sicuro di voler investire in questa azienda?\n1. Confermo\n2. Non confermo");
             int conferma; 
             scanf("%d", &conferma);
@@ -188,8 +210,15 @@ if (scelta == 1){
                         goto possibilinvestimenti; 
                     }
         }
+        else if (confermacontinuare == 2){
+            goto possibilinvestimenti;
+        }
+    }      
          if (sceltaimpresa == 4){
             printf("\n\nReMind\nSettore: Salute mentale e benessere\nApp di benessere mentale che offre strumenti di auto-aiuto, sessioni di meditazione guidata e supporto da parte di psicologi qualificati.\n ");
+            int confermacontinuare;
+            scanf("%d", confermacontinuare);
+            if (confermacontinuare == 1){
             printf("Sei sicuro di voler investire in questa azienda?\n1. Confermo\n2. Non confermo");
             int conferma; 
             scanf("%d", &conferma);
@@ -215,8 +244,12 @@ if (scelta == 1){
                     else if (conferma == 2){
                         goto possibilinvestimenti; 
                     }
+            }
+            else if (confermacontinuare == 2){
+                goto possibilinvestimenti;
+            }
         }
-}
+    }
         else if (scelta == 2){
     printf("\nEcco le possibilità di investimento create su misura per te.\nPremi il numero corrispondente all'azienda per ottenere più informazioni\n");
     printf("1. EduGames\n2. Safetech Guardian\n3. SustainableFashion\n4. CleanOcean Solutions");
@@ -672,6 +705,9 @@ if (scelta == 1){
                     }
         }
     }
+    else if (scelta == 6) {
+        generaguadagno(numeroinvestimenti);
+    }
 }
 
 void login (t_user *user){
@@ -681,10 +717,17 @@ void login (t_user *user){
 
     login:
     printf("\n\nUsername: ");
-    scanf(" %s", username);
+    scanf(" %s", &username);
     printf("Password: ");
-    scanf(" %s", password);
-    if (strcmp(username, user->username) == 0 && strcmp(password, user->password)== 0 ){
+    scanf(" %s", &password);
+    printf("UUUU-----%s--------%s---------UUUUUUU", username, password);
+
+
+    int d = strcmp(username, user->username);
+    printf("uuu %d", d);
+    int f = strcmp(password, user->password);
+    printf("uuu %d", f);
+    if (strcmp(username, user->username) == 1 && (strcmp(password, user->password))== 1 ){
             printf("\n\nBenvenuto nel tuo spazio personale.\n");
             sleep (1);
             codicesicurezza:
